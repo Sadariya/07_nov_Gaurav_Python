e2404e61a7d697ea6801d00a95803ffc9661b71c
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-12-08 10:16:35 +0530

    Update my_lib.py

--- Assignment/module 3/my_lib.py
+++ Assignment/module 3/my_lib.py
@@ -13,3 +13,30 @@ def user_list () :
         list1.append(list_item)
     return list1
 
+def user_dict ():
+    keys = []
+    values = []
+
+    n = int (input ("Enter the number of keys :- "))
+
+    for i in range (n):
+        key = input("Enter the key :- ")
+        value = input("Enter the value :- ")
+        keys.append(key)
+        values.append(value)
+    x = dict(zip (keys,values))
+    return x
+
+
+def error_handle ():
+    from builtins import int
+    while True:
+        try:
+            n=input("Enter the value : ")
+            n=int(n)
+            break
+        except Exception:
+            print("Invalid Input")
+        finally:
+            print("Finally Called")
+    print("Bye")
