485319fd8b68333be41e1b18955907cb7a6d4635
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-12-12 19:10:27 +0530

    Add files via upload

---
+++ Assignment/module 4/8_Write a python program to find the longest words.py
@@ -0,0 +1,11 @@
+def longest_word(filename):
+    with open(filename, 'r') as infile:
+              words = infile.read().split()
+    max_len = len(max(words, key=len))
+    return [word for word in words if len(word) == max_len]
+
+import os
+
+os.chdir('module 4')
+
+print(longest_word('3_Text.txt'))
