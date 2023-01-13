df9177bcaa8f18b814126207ad93d0671bb44f39
Sadariya <110012808+Sadariya@users.noreply.github.com>
2023-01-13 10:58:06 +0530

    Add files via upload

---
+++ task/7)_task/myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index (request):
+    fr = open('count.txt','r')
+
+    num = int(fr.read())
+    
+    fr.close()
+   
+    add(num)
+    
+    return render(request,'index.html',context={'number' : num})
+
+def add (number):
+
+    number = number + 1
+
+    fl=open('count.txt','w')
+
+    fl.write(f"{number}")
+
+    fl.close()
+
+
